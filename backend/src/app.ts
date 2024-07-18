8ad34b40c9ae57031f340e75bfd1d1feb865d3c7
Sayyed Muhammad Atif Ali <119723850+Atif-11@users.noreply.github.com>
2024-07-19 00:18:46 +0500

    All login and signup routes are updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import {config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 config();
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 // Only need this in the development mode
 app.use(morgan("dev"));
